5173026a073ebb462ef2eb49e4e8b253a681e4e0
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-01 17:57:07 +0200

    New default formatter for Gender code with twig template

--- src/Plugin/Field/FieldFormatter/GenderCodeDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/GenderCodeDefaultFormatter.php
@@ -5,7 +5,6 @@ namespace Drupal\ewp_core\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-// use Drupal\Core\Form\OptGroup;
 
 /**
  * Plugin implementation of the 'gender_code_default' formatter.
@@ -25,13 +24,13 @@ class GenderCodeDefaultFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $gender_codes = \ewp_core_get_human_sexes();
-    // Already a flat array
-    // $options = OptGroup::flattenOptions($gender_codes);
     $elements = [];
     foreach ($items as $delta => $item) {
       $value = $item->value;
-      // $elements[$delta] = ['#markup' => $options[$value]];
-      $elements[$delta] = ['#markup' => $gender_codes[$value]];
+      $elements[$delta] = [
+        '#theme' => 'ewp_gender_code_default',
+        '#value' => $gender_codes[$value],
+      ];
     }
     return $elements;
   }
--- src/Plugin/Field/FieldFormatter/GenderCodeSimpleFormatter.php
+++ src/Plugin/Field/FieldFormatter/GenderCodeSimpleFormatter.php
@@ -5,14 +5,13 @@ namespace Drupal\ewp_core\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-// use Drupal\Core\Form\OptGroup;
 
 /**
  * Plugin implementation of the 'gender_code_simple' formatter.
  *
  * @FieldFormatter(
  *   id = "gender_code_simple",
- *   label = @Translation("Simple (plain text)"),
+ *   label = @Translation("Simple (code only)"),
  *   field_types = {
  *     "gender_code"
  *   }
@@ -24,14 +23,11 @@ class GenderCodeSimpleFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $gender_codes = \ewp_core_get_human_sexes();
-    // Already a flat array
-    // $options = OptGroup::flattenOptions($gender_codes);
     $elements = [];
     foreach ($items as $delta => $item) {
       $value = $item->value;
-      // $elements[$delta] = ['#markup' => $options[$value]];
-      $elements[$delta] = ['#markup' => $gender_codes[$value]];
+      // print only the key which is the Gender code
+      $elements[$delta] = ['#markup' => $value];
     }
     return $elements;
   }
